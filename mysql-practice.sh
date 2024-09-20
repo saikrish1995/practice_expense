fa5ba8feece0a0bee3f78ee21ae21c3f8466dc2d
saikrish1995 <potlasaikrishna@gmail.com>
2024-09-20 13:44:16 +0100

    practice-mysql

--- mysql-practice.sh
+++ mysql-practice.sh
@@ -17,27 +17,36 @@ N="\e[0m"
 USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
-    echo -e "$R Please run this script with Super User privilages $N"
+    echo -e "$R Please run this script with Super User privilages $N" | tee -a $LOG_FILE
     exit 1
 fi
 
 VALIDATE(){
     if [ $? -ne 0 ]
     then
-        echo -e "$1 is $R failed $N"
+        echo -e "$1 is $R failed $N" | tee -a $LOG_FILE 
         exit 1
     else
-        echo -e "$1 is $G success $N"
+        echo -e "$1 is $G success $N" | tee -a $LOG_FILE
     fi
 }
 
-dnf install mysql-server -y
-VALIDATE "Installing MySQL Server"
+dnf install mysql-server -y &>>$LOG_FILE
+VALIDATE "Installing Mysql Server"
 
-systemctl enable mysqld
+systemctl enable mysqld &>>$LOG_FILE
+VALIDATE "Enabled Mysql server"
 
-systemctl start mysqld
+systemctl start mysqld &>>$LOG_FILE
+VALIDATE "Starting Mysql server"
 
-# mysql -h 172.31.104.4 -u root -pExpenseApp@1 -e
+mysql -h 172.31.104.4 -u root -pExpenseApp@1 -e 'show databases;' &>>$LOG_FILE
 
-# mysql_secure_installation --set-root-pass ExpenseApp@1
+if [ $? -ne 0 ]
+then
+    echo -e "$R Root Password Not set, setting up now $N" | tee -a $LOG_FILE
+    mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOG_FILE
+    VALIDATE "Mysql root password set"
+else
+    echo -e "Mysql root password already set.. $Y Skipping $N" | tee -a $LOG_FILE
+fi
